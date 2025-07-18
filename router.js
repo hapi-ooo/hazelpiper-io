ab13790d6161b3952f6fd401eb00b795a7329d7d
Hazel <hapi.csc@gmail.com>
2025-07-18 00:08:35 -0700

    added dynamic page title

--- router.js
+++ router.js
@@ -13,12 +13,18 @@ function removeAccentUnderline() {
   }
 }
 
+// won't work for strings with spaces..
+function capitalizeFirst(s) {
+  return s.charAt(0).toUpperCase() + s.slice(1, s.length);
+}
+
 function renderRoute() {
   removeAccentUnderline();
   const hash = location.hash.replace('#', '') || 'work';
   const content = routes[hash] || `<p>Page not found.</p>`;
   document.getElementById('content').innerHTML = content;
   document.getElementById(`link-${hash}`).classList.add('accent-underline');
+  document.title = `${capitalizeFirst(hash)} - Hazel Piper`
 }
 
 export function listenForRouting() {
